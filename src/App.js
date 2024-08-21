bf8a104d337d6c727e4c6552b1f1932b61dfaa29
Amber Wong <amber.wong32@gmail.com>
2024-08-21 19:20:05 +1000

    Fix background colour

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
+
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './components/HomePage';
 
 function App() {
-  const style = {
-    backgroundColor: '#3c5f8f',
-    height: '100vh'
-  }
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div style={style}>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
-    </div>
   );
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,19 +3,22 @@ import React from "react";
 import traffic from "../images/trafficcone.png"
 
 export default function HomePage() {
-    return (
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <img src={traffic} style={{ width: '600px', marginTop: '-100px' }} draggable={false}/>
-            <span style={{ fontFamily: 'Anton', fontSize: '4em', color: '#ff990a', marginTop: '-50px' }}>
-                Under Construction</span>
-        </div>
-    )
+  // const matches = useMediaQuery('(max-width:600px)')
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      {/* TODO: fix for mobile */}
+      <img src={traffic} style={{ minWidth: '100px', maxWidth: '600px', marginTop: '-100px' }} draggable={false} alt="Traffic Cone"/>
+      <span style={{ fontFamily: 'Anton', fontSize: '3em', color: '#ff990a', marginTop: '-50px' }}>
+        Under Construction</span>
+    </div>
+  )
 }
