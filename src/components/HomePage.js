9ba8f0a19c3097f2d69830c837bca48463f4d57f
Amber Wong <amber.wong32@gmail.com>
2024-08-26 18:44:46 +1000

    Create icon button link component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,40 +1,51 @@
 import React from "react";
 
 import Construction from "./Construction";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, ThemeProvider, Typography, createTheme } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import IconLink from "./IconLink";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Anton',
+  },
+});
+
 
 export default function HomePage() {
   return (
     // <Construction/>
-    <Box sx={{
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'flex-end'
-    }}>
-      <div style={{  marginRight: '20%' }}>
-        <span style={{ fontFamily: 'Anton', fontSize: '3em', color: '#3c5f8f' }}>Amber</span>
-        <div className="icon-grid" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
-            <div>
-              <a href="https://github.com/amborgw" target="_blank" aria-label="amborgw github link">
-                <GitHubIcon sx={{ fontSize: 33, color: '#3c5f8f', ":hover": {
-                  color: '#757575'
-                } }}/>
-              </a>
-            </div>
-            <div>
-              <a href="https://www.linkedin.com/in/amber-wong-67744920b/" target="_blank" aria-label="amborgw github link">
-                <LinkedInIcon sx={{ fontSize: 35, color: '#3c5f8f', ":hover": {
-                  color: '#757575'
-                } }}/>
-              </a>
-            </div>
+    <ThemeProvider theme={theme}>
+
+      <Box sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+      }}>
+        <div style={{  marginRight: '20%' }}>
+          <Typography style={{ fontSize: '3em', color: '#3c5f8f' }}>Amber</Typography>
+          <div className="icon-grid" style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+              <div>
+                <IconLink link="https://github.com/amborgw">
+                  <GitHubIcon sx={{ fontSize: 33, color: '#3c5f8f', ":hover": {
+                    color: '#757575'
+                  } }}/>
+                </IconLink>
+              </div>
+              <div>
+                <IconLink link="https://www.linkedin.com/in/amber-wong-67744920b/">
+                  <LinkedInIcon sx={{ fontSize: 35, color: '#3c5f8f', ":hover": {
+                    color: '#757575'
+                  } }}/>
+                </IconLink>
+              </div>
+          </div>
         </div>
-      </div>
-    </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
---
+++ src/components/IconLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function IconLink(props) {
+  return (
+    <a href={props.link} target="_blank" aria-label="amborgw github link">
+      {props.children}
+    </a>
+  )
+}
