9ee4ca934a1da95d2cb8af397a52d137aa696cd8
Amber Wong <amber.wong32@gmail.com>
2024-12-02 19:20:16 +1100

    Responsiveness

--- src/components/FeaturedItems.js
+++ src/components/FeaturedItems.js
@@ -19,38 +19,40 @@ export default function FeaturedItems() {
           src={profile}
           alt="profile"
         />
-        <Typography variant="h1">Hi, I'm Amber Wong</Typography>
-        <Box sx={{ marginTop: "3rem" }}>
-          <Typography variant="subtitle2">
-            I am a 3rd Year student at UNSW studying Computer Science and
-            Neuroscience.
-            <br />
-            <br />I love <i style={{ fontWeight: "bold" }}>creating</i> and{" "}
-            <i style={{ fontWeight: "bold" }}>curating</i> the world around me,
-            so you can find me making something or thinking of making something.
-            <br />
-            <br />
-            Please take a look around my{" "}
-            <i style={{ fontWeight: "bold" }}>
-              decorated corner of the internet
-            </i>
-          </Typography>
-        </Box>
-        <Box sx={{ marginTop: "3rem" }}>
-          <Typography variant="subtitle2">
-            Listening to{" "}
-            <i style={{ fontWeight: "bold" }}>Carnivore - Bear Attack!</i>
-            <br />
-            Reading{" "}
-            <i style={{ fontWeight: "bold" }}>The Bell Jar - Sylvia Plath</i>
-            <br />
-            Working on <i style={{ fontWeight: "bold" }}>this website</i>
-            <br />
-            {/* Obsessed with these{" "}
-              <i style={{ fontWeight: "bold" }}>3 memories of the month</i>
-              <br /> */}
-          </Typography>
-        </Box>
+        <Stack direction="column" spacing={2}>
+          <Typography variant="h1">Hi, I'm Amber Wong</Typography>
+          <Box sx={{ marginTop: "3rem" }}>
+            <Typography variant="subtitle2">
+              I am a 3rd Year student at UNSW studying Computer Science and
+              Neuroscience.
+              <br />
+              <br />I love <i style={{ fontWeight: "bold" }}>creating</i> and{" "}
+              <i style={{ fontWeight: "bold" }}>curating</i> the world around me,
+              so you can find me making something or thinking of making something.
+              <br />
+              <br />
+              Please take a look around my{" "}
+              <i style={{ fontWeight: "bold" }}>
+                decorated corner of the internet
+              </i>
+            </Typography>
+          </Box>
+          <Box sx={{ marginTop: "3rem" }}>
+            <Typography variant="subtitle2">
+              Listening to{" "}
+              <i style={{ fontWeight: "bold" }}>Carnivore - Bear Attack!</i>
+              <br />
+              Reading{" "}
+              <i style={{ fontWeight: "bold" }}>The Bell Jar - Sylvia Plath</i>
+              <br />
+              Working on <i style={{ fontWeight: "bold" }}>this website</i>
+              <br />
+              {/* Obsessed with these{" "}
+                <i style={{ fontWeight: "bold" }}>3 memories of the month</i>
+                <br /> */}
+            </Typography>
+          </Box>
+        </Stack>
       </Stack>
     </CardContainer>
   );
