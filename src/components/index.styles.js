47e096327fee59f7704c229f2b7c21ce9c00e57f
Amber Wong <amber.wong32@gmail.com>
2024-12-02 18:56:05 +1100

    Run deploy again

--- src/components/index.styles.js
+++ src/components/index.styles.js
@@ -3,13 +3,11 @@ import styled from "styled-components";
 import { Box } from "@mui/material";
 
 const CardContainer = styled(Box)`
-    && {
-        background: rgba(244, 244, 244, 0.9);
-        max-width: 55rem;
-        border-radius: 1rem;
-        margin: 2rem;
-        padding: 2rem;
-    }
+  background: rgba(244, 244, 244, 0.9);
+  max-width: 55rem;
+  border-radius: 1rem;
+  margin: 2rem;
+  padding: 2rem;
 `;
 
-export { CardContainer}
+export { CardContainer };
